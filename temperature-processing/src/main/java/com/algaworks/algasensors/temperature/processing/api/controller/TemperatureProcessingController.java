51b3da95d44c153358101409f7d1d4e0316d3b21
Tony Costa <tonyblack10@gmail.com>
2025-04-20 01:08:01 -0300

    add TSID serialization support and integrate RabbitMQ messaging in temperature processing

--- temperature-processing/src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
+++ temperature-processing/src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -1,44 +1,58 @@
 package com.algaworks.algasensors.temperature.processing.api.controller;
 
+import static com.algaworks.algasensors.temperature.processing.infrastructure.rabbitmq.RabbitMQConfig.FANOUT_EXCHANGE_NAME;
+
 import com.algaworks.algasensors.temperature.processing.api.model.TemperatureLogOutput;
 import com.algaworks.algasensors.temperature.processing.common.IdGenerator;
 import io.hypersistence.tsid.TSID;
+import java.time.OffsetDateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.time.OffsetDateTime;
 
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/temperatures/data")
 public class TemperatureProcessingController {
 
-    private static final Logger log = LoggerFactory.getLogger(TemperatureProcessingController.class);
+  private static final Logger log = LoggerFactory.getLogger(TemperatureProcessingController.class);
 
-    @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
-    public void data(@PathVariable TSID sensorId, @RequestBody String input) {
-        if (input == null || input.isBlank()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Input cannot be null or empty");
-        }
+  private final RabbitTemplate rabbitTemplate;
 
-        Double temperature;
+  public TemperatureProcessingController(RabbitTemplate rabbitTemplate) {
+    this.rabbitTemplate = rabbitTemplate;
+  }
 
-        try {
-            temperature = Double.parseDouble(input);
-        } catch (NumberFormatException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid temperature value");
-        }
-
-        var temperatureLogOutput = TemperatureLogOutput.builder()
-                .id(IdGenerator.generateTimeBasedUUID())
-                .sensorId(sensorId)
-                .value(temperature)
-                .registeredAt(OffsetDateTime.now())
-                .build();
-
-        log.info("temperatureLogOutput: {}", temperatureLogOutput);
+  @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
+  public void data(@PathVariable TSID sensorId, @RequestBody String input) {
+    if (input == null || input.isBlank()) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Input cannot be null or empty");
     }
+
+    Double temperature;
+
+    try {
+      temperature = Double.parseDouble(input);
+    } catch (NumberFormatException e) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid temperature value");
+    }
+
+    var temperatureLogOutput = TemperatureLogOutput.builder()
+        .id(IdGenerator.generateTimeBasedUUID())
+        .sensorId(sensorId)
+        .value(temperature)
+        .registeredAt(OffsetDateTime.now())
+        .build();
+
+    log.info("temperatureLogOutput: {}", temperatureLogOutput);
+
+    rabbitTemplate.convertAndSend(FANOUT_EXCHANGE_NAME, "", temperatureLogOutput);
+  }
 }
--- temperature-processing/src/main/java/com/algaworks/algasensors/temperature/processing/infrastructure/rabbitmq/RabbitMQConfig.java
+++ temperature-processing/src/main/java/com/algaworks/algasensors/temperature/processing/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -1,14 +1,18 @@
 package com.algaworks.algasensors.temperature.processing.infrastructure.rabbitmq;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+  public static final String FANOUT_EXCHANGE_NAME = "temperature-processing.temperature-received.v1.e";
 
   @Bean
   public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
@@ -16,9 +20,14 @@ public class RabbitMQConfig {
   }
 
   @Bean
+  public Jackson2JsonMessageConverter messageConverter(ObjectMapper objectMapper) {
+    return new Jackson2JsonMessageConverter(objectMapper);
+  }
+
+  @Bean
   public FanoutExchange exchange() {
     return ExchangeBuilder
-        .fanoutExchange("temperature-processing.temperature-received.v1.e")
+        .fanoutExchange(FANOUT_EXCHANGE_NAME)
         .build();
   }
 
